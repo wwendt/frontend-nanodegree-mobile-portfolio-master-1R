3925ff99240f233118ab43e741150b974ef8b5e1
Wes <westin.wendt2@gmail.com>
2016-11-09 15:16:24 -0800

    message

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+
+Things I did:
+
+-changed to document.getElementsByClassName()
+-oved newwidth and dex variables outside of the loop
+-created new variable randomPizzaContainer
+-moved several variables out of the loop
+-broke up calculation into a few different lines
+-inlined a lot of css
+-inlined a lot of javascript
--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    var dx = determineDx(document.getElementByClassName(".randomPizzaContainer")[i], size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+    var randomPizzaContainer = document.getElementsByClassName('randomPizzaContainer');
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      randomPizzaContainer[i].style.width = newwidth;
     }
   }
 
